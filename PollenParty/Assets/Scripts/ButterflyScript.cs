82a00d4a4ffff256a2c3c7ddd861dc1e62fce46f
fournickle <merlin.viernickel@gmail.com>
2026-05-30 16:38:56 +0200

    final flapping movement

--- PollenParty/Assets/Scripts/ButterflyScript.cs
+++ PollenParty/Assets/Scripts/ButterflyScript.cs
@@ -7,6 +7,8 @@ public class ButterflyScript : MonoBehaviour
     public InputActionAsset InputActions;
 
     [SerializeField] private float flapForce = 5f;
+    [SerializeField] private float horizontalVelocityDecay = 0.95f;
+    [SerializeField] private float maxVerticalVelocity = 4f;
 
     private InputAction m_flapLeft;
     private InputAction m_flapRight;
@@ -35,17 +37,29 @@ public class ButterflyScript : MonoBehaviour
     {
         if (m_flapLeft.WasPressedThisFrame())
         {
+            Debug.Log("Flap Left");
             Flap(-1);
         }
         else if (m_flapRight.WasPressedThisFrame())
         {
+            Debug.Log("Flap Right");
             Flap(1);
         }
+
+        // Apply horizontal velocity decay
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x *= horizontalVelocityDecay;
+        rb.linearVelocity = velocity;
+
+        // Clamp vertical velocity
+        velocity = rb.linearVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -maxVerticalVelocity, maxVerticalVelocity);
+        rb.linearVelocity = velocity;
     }
 
     private void Flap(int direction)
     {
-        Vector2 force = new Vector2(direction * flapForce, flapForce);
+        Vector2 force = new Vector2(direction * flapForce, flapForce/2);
         rb.AddForce(force, ForceMode2D.Impulse);
     }
 }
